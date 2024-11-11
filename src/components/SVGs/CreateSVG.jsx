ea9d7412772d441d2bae4514a303eb68c94f24ef
MDNURAAZAM <md.nura.azam@g.bracu.ac.bd>
2024-11-11 19:59:09 +0600

    fix svg classname

--- src/components/SVGs/CreateSVG.jsx
+++ src/components/SVGs/CreateSVG.jsx
@@ -10,9 +10,9 @@ const CreateSVG = () => {
       stroke="currentColor"
     >
       <path
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        stroke-width="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
         d="M12 4v16m8-8H4"
       />
     </svg>
