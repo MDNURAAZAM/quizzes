fd683ea70b009f4970dcf84792184c736109ec99
MDNURAAZAM <md.nura.azam@g.bracu.ac.bd>
2024-11-11 21:15:55 +0600

    add redux

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { store } from "./app/store.js";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
@@ -12,7 +14,9 @@ createRoot(document.getElementById("root")).render(
         v7_startTransition: true,
       }}
     >
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </BrowserRouter>
   </StrictMode>
 );
