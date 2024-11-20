6794ac501441b6080e2870c012caf00abeb2378a
MDNURAAZAM <md.nura.azam@g.bracu.ac.bd>
2024-11-20 19:03:34 +0600

    add edit delete button for quiz card

--- src/components/Dashboard/QuizCard.jsx
+++ src/components/Dashboard/QuizCard.jsx
@@ -1,3 +1,5 @@
+import DeleteSVG from "../SVGs/DeleteSVG";
+import EditSVG from "../SVGs/EditSVG";
 import QuizSVG from "../SVGs/QuizSVG";
 
 const QuizCard = ({ quiz }) => {
@@ -5,7 +7,17 @@ const QuizCard = ({ quiz }) => {
   return (
     <div className="h-full bg-white p-6 rounded-lg shadow-sm border border-gray-200 group cursor-pointer">
       <div className="text-buzzr-purple mb-4 group-hover:scale-105 transition-all">
-        <QuizSVG />
+        <div className="flex items-center justify-between">
+          <QuizSVG />
+          <div className="flex items-center justify-between gap-2">
+            <button>
+              <EditSVG />
+            </button>
+            <button>
+              <DeleteSVG />
+            </button>
+          </div>
+        </div>
       </div>
       <h3 className="font-semibold text-lg mb-2 group-hover:scale-105 transition-all">
         {title} Quiz
