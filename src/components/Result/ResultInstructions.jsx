7b0e53e14a7e6e96e641c87d4707fd5350f653a8
MDNURAAZAM <md.nura.azam@g.bracu.ac.bd>
2024-11-23 22:49:30 +0600

    fix result instructions

--- src/components/Result/ResultInstructions.jsx
+++ src/components/Result/ResultInstructions.jsx
@@ -2,7 +2,7 @@ import { TiTick } from "react-icons/ti";
 
 const ResultInstructions = () => {
   return (
-    <div className="grid grid-cols-3 p-3 gap-4">
+    <div className="grid grid-cols-3 mb-2 mx-6 gap-4 bg-gray-100 rounded-md">
       <div className=" flex items-center justify-start gap-2 p-2">
         <div className="w-5 h-5 rounded-full border bg-green-200"></div>
         <div className="">correct answer</div>
